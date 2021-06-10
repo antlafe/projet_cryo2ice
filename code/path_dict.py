3bd82367e926ccecd9fc5b213f0237468bf4badf
antlafe <antoine.laforge8@gmail.com>
2021-06-10 12:56:01 +0000

    adding paths

--- code/path_dict.py
+++ code/path_dict.py
@@ -5,15 +5,21 @@
 PATH_DICT = {
 
     # Repertory where output pikle files of aligned data will be stored
-    'PATH_OUT': '/home/antlafe/Documents/work/data/CRYO2ICE/Cryo2Ice/',
-    # on HAL: /work/ALT/odatis/seaice/users/laforga/data/CRYO2ICE/Cryo2Ice/'
+    # on PC
+    #'PATH_OUT': '/home/antlafe/Documents/work/data/CRYO2ICE/Cryo2Ice/',
+    # on HAL:
+    'PATH_OUT': '/work/ALT/odatis/seaice/users/laforga/data/CRYO2ICE/Cryo2Ice/',
 
     # Repertory where all tracks from mission will be found
-    'PATH_DATA': '/home/antlafe/Documents/work/data/','
-    # on HAL: /work/ALT/odatis/seaice/users/laforga/data/'
+    # On PC
+    #'PATH_DATA': '/home/antlafe/Documents/work/data/','
+    # On HAL:
+    'PATH_DATA': '/work/ALT/odatis/seaice/users/laforga/data/',
 
     # Repertory where only collocated tracks will be found
-    'PATH_COLLOC': '/home/antlafe/Documents/work/data/CRYO2ICE/',
-    # on HAL: /work/ALT/odatis/seaice/users/laforga/data/CRYO2ICE/'
+    # on PC
+    #'PATH_COLLOC': '/home/antlafe/Documents/work/data/CRYO2ICE/',
+    # on HAL:
+    'PATH_COLLOC': '/work/ALT/odatis/seaice/users/laforga/data/CRYO2ICE/',
 
 }
